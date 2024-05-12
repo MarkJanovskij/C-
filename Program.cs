a262fb9292d2d8f0c32062ccdada3edd5e5546cb
Mark <markyanovskiy@gmail.com>
2024-05-12 14:25:37 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using static System.Console;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Testing WriteLine
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World I am Mark Janovskij");
+            ReadKey();
+            TestWriteMethod();
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// Testing Write
+        /// </summary>
+        /// <param name="arg"></param>
+        static void TestWriteMethod()
+        {
+            Write("Hello ");
+            Write("WORLD ");
+            Write("!!!! ");
+            ReadKey();
+        }
+    }
+}
